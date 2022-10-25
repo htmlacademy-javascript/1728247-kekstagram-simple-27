e0211ac550e059e70ed5b776f220cb4a3bd8b0de
MarieKrymova <lifeandfreedom@gmail.com>
2022-10-25 17:16:40 +0300

    исправления для ESLint

--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
 function getRandomNumber (min, max) {
-    if (typeof min !== 'number' || typeof max !== 'number' || min < 0 || max < 0) {
-        return NaN;
-    }
-    return Math.floor(Math.random() * (max - min + 1)) + min;
+  if (typeof min !== 'number' || typeof max !== 'number' || min < 0 || max < 0) {
+    return NaN;
+  }
+  return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
 function checkStringLength (string, maxLength) {
