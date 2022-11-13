f9e0f32465b276ac90448db776fe409cdc5baca7
MarieKrymova <lifeandfreedom@gmail.com>
2022-11-13 18:38:03 +0300

    внесены исправления согласно комментариям наставника

--- js/main.js
+++ js/main.js
@@ -1,32 +1,16 @@
-/* eslint-disable prefer-const */
-function getRandomNumber (a, b) {
-  if (typeof a !== 'number' || typeof b !== 'number' || a < 0 || b < 0) {
-    return NaN;
-  }
-  const min = Math.ceil(Math.min(a, b));
-  const max = Math.floor(Math.max(a, b));
-  return Math.floor(Math.random() * (max - min + 1) + min);
-}
+const PHOTO_AMOUNT = 25;
 
-function checkStringLength (string, maxLength) {
-  return string.length <= maxLength;
-}
-checkStringLength('', 140);
-
-const VARIANTS = 25;
-const ID_QUANTITY = 25;
-
-const LikesCount = {
+const Like = {
   MIN: 15,
   MAX: 200,
 };
 
-const CommentsCount = {
+const Comment = {
   MIN: 0,
   MAX: 200,
 };
 
-const photoDescriptionVariants = [
+const DESCRIPTIONS = [
   'Смотрите, какой очаровательный котик!',
   'Найдите на фото моего пушистика))',
   'Кто это тут такой сладкий мальчик??!!',
@@ -42,28 +26,36 @@ const photoDescriptionVariants = [
   'Кто у нас тут молодец? Вот этот молодец!',
 ];
 
-const getRandomElement = (array) =>
-  array[getRandomNumber(0, array.length - 1)];
-
-const generateIndexesArray = (quantity) => {
-  let indexesArray = [];
-  for (let i = 1; i <= quantity; i++) {
-    indexesArray.push(i);
+const getRandomNumber = (a, b) => {
+  if (typeof a !== 'number' || typeof b !== 'number' || a < 0 || b < 0) {
+    return NaN;
   }
-  return indexesArray;
+  const min = Math.ceil(Math.min(a, b));
+  const max = Math.floor(Math.max(a, b));
+  return Math.floor(Math.random() * (max - min + 1) + min);
 };
 
-let indexes = generateIndexesArray(ID_QUANTITY);
+const getRandomElement = (array) => array[getRandomNumber(0, array.length - 1)];
 
-function allPhotosDescriptions () {
-  let publishedPhotoDescription = [];
-  for (let i = 0; i <= VARIANTS - 1; i++) {
-    publishedPhotoDescription.push({id: indexes[i],
-      url: `photos/${indexes[i]}.jpg`,
-      description: getRandomElement(photoDescriptionVariants),
-      likes: getRandomNumber(LikesCount.MIN, LikesCount.MAX),
-      comments: getRandomNumber(CommentsCount.MIN, CommentsCount.MAX),});}
-  return publishedPhotoDescription;
-}
+const checkStringLength = (string, maxLength) => string.length <= maxLength;
+checkStringLength('', 140);
+
+const generatePhoto = (photoId) => ({
+  id: photoId,
+  url: `photos/${photoId}.jpg`,
+  description: getRandomElement(DESCRIPTIONS),
+  likes: getRandomNumber(Like.MIN, Like.MAX),
+  comments: getRandomNumber(Comment.MIN, Comment.MAX),
+});
+
+const getPhoto = (amount) => {
+  const items = [];
+  for (let i = 0; i < amount; i++) {
+    const photo = generatePhoto(i + 1);
+    items.push(photo);}
+  return items;
+};
+
+const photos = getPhoto(PHOTO_AMOUNT);
 // eslint-disable-next-line no-console
-console.log(allPhotosDescriptions());
+console.log(photos);
