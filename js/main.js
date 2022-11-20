2a03fbde766e74e5564eda8c0508388177179221
MarieKrymova <lifeandfreedom@gmail.com>
2022-11-20 12:23:19 +0300

    на страницу добавлены миниатюры других пользователей

--- js/main.js
+++ js/main.js
@@ -1,7 +1,5 @@
-import {getPhoto, PHOTO_AMOUNT} from './data.js';
 import './form.js';
 import './slider.js';
+import './miniatures.js';
 
-const photos = getPhoto(PHOTO_AMOUNT);
-// eslint-disable-next-line no-console
-console.log(photos);
+
